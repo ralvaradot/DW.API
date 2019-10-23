333c368fe6706d7c9a9e2f142f4f46ab9b6e602f
Roberto Alvarado <ralvaradot@hotmail.com>
2019-10-23 12:19:35 -0500

    Se crean los modelos en el proyecto Entity

---
+++ DW.API.Entity/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DW.API.Entity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        // Aqui creamos las propiedades adicionales a la tabla de usuario
+        // AspNetUsers
+    }
+}
